1dd579763aa568d341196eff8515c2931ff4f3ff
Nizar Beriane <nizarberiane43@gmail.com>
2024-11-04 09:32:33 +0100

    edit

--- movie detailes.js
+++ movie detailes.js
@@ -4,16 +4,14 @@ const closeBtn = document.getElementById("closeBtn");
 
 hamburger.addEventListener("click", () => {
   sidePanel.classList.add("open");
-
 });
 
 closeBtn.addEventListener("click", () => {
   sidePanel.classList.remove("open");
 });
-let movieList = JSON.parse(localStorage.getItem("movieList")) || [];
+let movieList = JSON.parse(localStorage.getItem("movieList"));
 let movieContainer = document.querySelector(".movie-details");
-let movie = JSON.parse(localStorage.getItem("movie")) || [];
-console.log(movie);
+let movieId = JSON.parse(localStorage.getItem("movie"));
 
 let title = document.querySelector(".movie-title h1");
 let rating = document.querySelector(".movie-rating p");
@@ -22,7 +20,23 @@ let type = document.querySelector(".movie-type");
 let country = document.querySelector(".movie-country");
 let year = document.querySelector(".movie-release");
 let production = document.querySelector(".movie-production");
-let discription = document.querySelector(".movie-discription");
-console.log(title, rating, genre, type, country, year, production, discription);
-let movieId = localStorage.getItem("movie");
-console.log(movieId);
+let discription = document.querySelector(".movie-discription  p");
+let poster = document.querySelector(".movie-poster img");
+
+movieList.forEach((movie) => {
+  if (movie.id == movieId) {
+    title.textContent = movie.name;
+    console.log(title.textContent);
+    console.log(movie.rating);
+    rating.textContent = movie.rating;
+    genre.textContent = `genre: ${movie.genre}`;
+    type.textContent = `type: ${movie.type}`;
+    country.textContent = `country: ${movie.country}`;
+    year.textContent = `release: ${movie.year}`;
+    production.textContent = `production: ${movie.production}`;
+    console.log(movie.description);
+    discription.textContent = movie.description;
+
+    poster.src = movie.poster;
+  }
+});
